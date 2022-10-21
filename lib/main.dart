fe732a523eece48c686ddf431a5ec2c05b89d648
Maxime <maxime.dubost.75@gmail.com>
2022-10-21 10:28:48 +0200

    Add related networks (metro, RER, Transiliens, tramway) + Use TextTheme

--- lib/main.dart
+++ lib/main.dart
@@ -62,6 +62,35 @@ class _MyHomePageState extends State<MyHomePage> {
   List<Line> _data = Data.beyond2030;
 
   ///
+  /// Build the main page
+  ///
+  @override
+  Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
+    double displayWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: buildAppBar(textTheme),
+      body: ListView(
+        physics: const BouncingScrollPhysics(),
+        children: [
+          const SizedBox(height: 20),
+          buildLargeTitle("Lignes du réseau", textTheme),
+          const SizedBox(height: 4),
+          buildNetworkLineList(_data, displayWidth, textTheme),
+          buildDivider(),
+          const SizedBox(height: 4),
+          buildLargeTitle("Réseaux connexes", textTheme),
+          buildMetroList(textTheme),
+          buildRERTransilienList(textTheme),
+          buildTramwayList(textTheme, _period),
+        ],
+      ),
+      bottomNavigationBar: buildBottomNavigationBar(displayWidth, textTheme),
+    );
+  }
+
+  ///
   /// Change period of time (between 2022 and 2030+)
   ///
   void _changePeriod(double period) {
@@ -102,45 +131,46 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   ///
-  /// Build the main page
+  /// Build a divider
   ///
-  @override
-  Widget build(BuildContext context) {
-    // TextTheme textTheme = Theme.of(context).textTheme;
-    double displayWidth = MediaQuery.of(context).size.width;
+  Divider buildDivider() {
+    return const Divider(height: 16, indent: 75, endIndent: 75, thickness: 1);
+  }
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          widget.title,
-          style: const TextStyle(
-            fontFamily: "Parisine",
-            fontWeight: FontWeight.bold,
-            fontSize: 24,
-            color: Colors.black,
-          ),
+  ///
+  /// Build the app bar of the main page
+  ///
+  AppBar buildAppBar(TextTheme textTheme) {
+    return AppBar(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          bottom: Radius.circular(16),
         ),
-        backgroundColor: Colors.white,
       ),
-      body: ListView(
-        physics: const BouncingScrollPhysics(),
-        children: [
-          buildLineList(_data, displayWidth),
-          Container(
-            // TODO : Add secondary lines
-            height: 170,
-            color: Colors.transparent,
-          )
-        ],
-      ),
-      floatingActionButton: buildBottomAppBar(displayWidth),
+      centerTitle: true,
+      title: Text(widget.title, style: textTheme.titleMedium),
+      backgroundColor: Colors.white,
+    );
+  }
+
+  ///
+  /// Build a large title
+  ///
+  Widget buildLargeTitle(String title, TextTheme textTheme) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 24),
+      child: Text(title, style: textTheme.titleLarge),
     );
   }
 
   ///
   /// Build the list of main metro lines
   ///
-  ListView buildLineList(List<Line> lineList, double displayWidth) =>
+  ListView buildNetworkLineList(
+    List<Line> lineList,
+    double displayWidth,
+    TextTheme textTheme,
+  ) =>
       ListView.builder(
         shrinkWrap: true,
         physics: const NeverScrollableScrollPhysics(),
@@ -157,13 +187,18 @@ class _MyHomePageState extends State<MyHomePage> {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Container(
+                    height: 40,
                     padding: const EdgeInsets.fromLTRB(0, 0, 4, 0),
                     child: Image.asset("assets/images/lines/metro/m.png"),
                   ),
-                  Image.asset(line.icon),
+                  SizedBox(
+                    height: 40,
+                    child: Image.asset(line.icon),
+                  ),
                   line.terminusList.length <= 2
-                      ? buildLineTitle20(line.terminusList)
-                      : buildLineTitle16(line.terminusList)
+                      ? buildLineTitle(line.terminusList, textTheme)
+                      : buildLineTitleManySections(
+                          line.terminusList, textTheme),
                 ],
               ),
             ),
@@ -174,22 +209,13 @@ class _MyHomePageState extends State<MyHomePage> {
   ///
   /// Build each title of the list of main metro lines (fontSize 20)
   ///
-  Container buildLineTitle20(List<String> terminusList) {
+  Container buildLineTitle(List<String> terminusList, TextTheme textTheme) {
     List<Widget> titleList = [];
     for (String element in terminusList) {
-      titleList.add(
-        Text(
-          element,
-          style: const TextStyle(
-            fontFamily: "Parisine",
-            fontSize: 20,
-            color: Color.fromRGBO(0, 0, 84, 1),
-          ),
-        ),
-      );
+      titleList.add(Text(element, style: textTheme.labelMedium));
     }
     return Container(
-      height: 72,
+      height: 64,
       margin: const EdgeInsets.fromLTRB(16, 0, 0, 0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -202,22 +228,16 @@ class _MyHomePageState extends State<MyHomePage> {
   ///
   /// Build each title of the list of main metro lines (fontSize 20)
   ///
-  Container buildLineTitle16(List<String> terminusList) {
+  Container buildLineTitleManySections(
+    List<String> terminusList,
+    TextTheme textTheme,
+  ) {
     List<Widget> titleList = [];
     for (String element in terminusList) {
-      titleList.add(
-        Text(
-          element,
-          style: const TextStyle(
-            fontFamily: "Parisine",
-            fontSize: 16,
-            color: Color.fromRGBO(0, 0, 84, 1),
-          ),
-        ),
-      );
+      titleList.add(Text(element, style: textTheme.labelSmall));
     }
     return Container(
-      height: 72,
+      height: 64,
       margin: const EdgeInsets.fromLTRB(16, 0, 0, 0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -230,55 +250,234 @@ class _MyHomePageState extends State<MyHomePage> {
   ///
   /// Build bottom app bar with period and slider
   ///
-  Container buildBottomAppBar(double displayWidth) {
+  Container buildBottomNavigationBar(double displayWidth, TextTheme textTheme) {
     return Container(
-      // margin: EdgeInsets.all(displayWidth * .05),
-      margin: const EdgeInsets.fromLTRB(32, 0, 0, 0),
       height: 120,
       decoration: BoxDecoration(
         color: Colors.white,
         boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(.1),
-            blurRadius: 30,
-            offset: const Offset(0, 10),
-          ),
+          BoxShadow(color: Colors.black.withOpacity(.25), blurRadius: 8),
         ],
-        borderRadius: BorderRadius.circular(40),
+        border: Border.all(color: Colors.grey.shade300),
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(16),
+          topRight: Radius.circular(16),
+        ),
       ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(
-            padding: const EdgeInsets.symmetric(
-              vertical: 4,
-              horizontal: 16,
-            ),
-            color: colorPrimary,
-            child: Text(
-              _period.toStringAsFixed(0) == "2031"
-                  ? "> 2030"
-                  : _period.toStringAsFixed(0),
-              style: const TextStyle(
-                fontFamily: "Parisine",
-                fontSize: 36,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          Slider.adaptive(
-            activeColor: colorAccent,
-            thumbColor: Colors.white,
-            value: _period,
-            onChanged: (newValue) {
-              _changePeriod(newValue);
-            },
-            min: 2022.0,
-            max: 2031.0,
-          ),
+          buildPeriodDisplay(textTheme),
+          buildSlider(displayWidth),
         ],
       ),
+    );
+  }
+
+  ///
+  /// Build the display of the period
+  ///
+  Widget buildPeriodDisplay(TextTheme textTheme) {
+    return Container(
+      width: 110,
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      color: colorPrimary,
+      child: Text(
+        _period.toStringAsFixed(0) == "2031"
+            ? "> 2030"
+            : _period.toStringAsFixed(0),
+        textAlign: TextAlign.center,
+        style: textTheme.labelLarge,
+      ),
+    );
+  }
+
+  ///
+  /// Build period slider
+  ///
+  Widget buildSlider(double displayWidth) {
+    return SizedBox(
+      width: displayWidth * .9,
+      child: Slider.adaptive(
+        activeColor: colorAccent,
+        thumbColor: Colors.white,
+        value: _period,
+        min: 2022.0,
+        max: 2031.0,
+        onChanged: (newValue) {
+          _changePeriod(newValue);
+        },
+      ),
+    );
+  }
+
+  ///
+  /// Build the list of the secondary metro lines
+  ///
+  Container buildRelatedNetworkList(List<String> imageAssetList) {
+    List<Widget> children = [];
+    for (var imageAsset in imageAssetList) {
+      children.add(
+        SizedBox(
+          height: 50,
+          child: IconButton(
+            splashRadius: 24,
+            onPressed: () {},
+            icon: Image.asset(imageAsset),
+          ),
+        ),
+      );
+    }
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+      height: 100,
+      child: GridView.count(
+        physics: const NeverScrollableScrollPhysics(),
+        crossAxisCount: 7,
+        children: children,
+      ),
+    );
+  }
+
+  ///
+  /// Build the related metro network
+  ///
+  Column buildMetroList(TextTheme textTheme) {
+    var metroList = [
+      "assets/images/lines/metro/1.png",
+      "assets/images/lines/metro/2.png",
+      "assets/images/lines/metro/3.png",
+      "assets/images/lines/metro/3bis.png",
+      "assets/images/lines/metro/4.png",
+      "assets/images/lines/metro/5.png",
+      "assets/images/lines/metro/6.png",
+      "assets/images/lines/metro/7.png",
+      "assets/images/lines/metro/7bis.png",
+      "assets/images/lines/metro/8.png",
+      "assets/images/lines/metro/9.png",
+      "assets/images/lines/metro/10.png",
+      "assets/images/lines/metro/12.png",
+      "assets/images/lines/metro/13.png",
+    ];
+
+    return Column(
+      children: [
+        const SizedBox(height: 16),
+        Row(
+          children: [
+            Container(
+              margin: const EdgeInsets.fromLTRB(24, 0, 8, 0),
+              child: Text("Métro", style: textTheme.titleSmall),
+            ),
+            SizedBox(
+              height: 24,
+              child: Image.asset("assets/images/lines/metro/m.png"),
+            ),
+          ],
+        ),
+        const SizedBox(height: 4),
+        buildRelatedNetworkList(metroList),
+      ],
+    );
+  }
+
+  ///
+  /// Build the RER and Transilien related network
+  ///
+  Widget buildRERTransilienList(TextTheme textTheme) {
+    var rerTransilienList = [
+      "assets/images/lines/rer/a.png",
+      "assets/images/lines/rer/b.png",
+      "assets/images/lines/rer/c.png",
+      "assets/images/lines/rer/d.png",
+      "assets/images/lines/rer/e.png",
+      "assets/images/lines/transilien/h.png",
+      "assets/images/lines/transilien/j.png",
+      "assets/images/lines/transilien/k.png",
+      "assets/images/lines/transilien/l.png",
+      "assets/images/lines/transilien/n.png",
+      "assets/images/lines/transilien/p.png",
+      "assets/images/lines/transilien/r.png",
+      "assets/images/lines/transilien/u.png",
+    ];
+
+    return Column(
+      children: [
+        const SizedBox(height: 16),
+        Row(
+          children: [
+            Container(
+              margin: const EdgeInsets.fromLTRB(24, 0, 8, 0),
+              child: Text("RER et Transiliens", style: textTheme.titleSmall),
+            ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(0, 0, 4, 0),
+              height: 24,
+              child: Image.asset("assets/images/lines/rer/rer.png"),
+            ),
+            SizedBox(
+              height: 24,
+              child:
+                  Image.asset("assets/images/lines/transilien/transilien.png"),
+            ),
+          ],
+        ),
+        const SizedBox(height: 4),
+        buildRelatedNetworkList(rerTransilienList),
+      ],
+    );
+  }
+
+  ///
+  /// Build the tramway related network
+  ///
+  Widget buildTramwayList(TextTheme textTheme, double period) {
+    var tramwayList = [
+      "assets/images/lines/tramway/t1.png",
+      "assets/images/lines/tramway/t2.png",
+      "assets/images/lines/tramway/t3a.png",
+      "assets/images/lines/tramway/t3b.png",
+      "assets/images/lines/tramway/t4.png",
+      "assets/images/lines/tramway/t5.png",
+      "assets/images/lines/tramway/t6.png",
+      "assets/images/lines/tramway/t7.png",
+      "assets/images/lines/tramway/t8.png",
+      "assets/images/lines/tramway/t9.png",
+    ];
+    if (period.toStringAsFixed(0) == "2022") {
+      tramwayList.addAll([
+        "assets/images/lines/tramway/t11.png",
+        "assets/images/lines/tramway/t13.png",
+      ]);
+    } else {
+      tramwayList.addAll([
+        "assets/images/lines/tramway/t10.png",
+        "assets/images/lines/tramway/t11.png",
+        "assets/images/lines/tramway/t12.png",
+        "assets/images/lines/tramway/t13.png",
+      ]);
+    }
+
+    return Column(
+      children: [
+        const SizedBox(height: 16),
+        Row(
+          children: [
+            Container(
+              margin: const EdgeInsets.fromLTRB(24, 0, 8, 0),
+              child: Text("Tramways", style: textTheme.titleSmall),
+            ),
+            SizedBox(
+              height: 24,
+              child: Image.asset("assets/images/lines/tramway/t.png"),
+            ),
+          ],
+        ),
+        const SizedBox(height: 4),
+        buildRelatedNetworkList(tramwayList),
+      ],
     );
   }
 }
--- lib/theme/theme_constants.dart
+++ lib/theme/theme_constants.dart
@@ -2,25 +2,86 @@ import 'package:flutter/material.dart';
 
 const colorPrimary = Color.fromRGBO(0, 0, 84, 1);
 const colorAccent = Color.fromRGBO(9, 0, 129, 1);
+const parisineFont = "Parisine";
 
 ThemeData lightTheme = ThemeData(
   brightness: Brightness.light,
   primaryColor: colorPrimary,
-  floatingActionButtonTheme: const FloatingActionButtonThemeData(
-    backgroundColor: colorAccent,
-  ),
-  elevatedButtonTheme: ElevatedButtonThemeData(
-    style: ButtonStyle(
-      padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-        const EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
-      ),
-      shape: MaterialStateProperty.all<OutlinedBorder>(
-        RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20.0),
-        ),
-      ),
-      backgroundColor: MaterialStateProperty.all<Color>(colorAccent),
+  textTheme: const TextTheme(
+    ///
+    /// Titles
+    ///
+    titleLarge: TextStyle(
+      fontFamily: parisineFont,
+      fontWeight: FontWeight.bold,
+      fontSize: 24,
+      color: Colors.black,
     ),
+    titleMedium: TextStyle(
+      fontFamily: parisineFont,
+      fontWeight: FontWeight.bold,
+      fontSize: 20,
+      color: colorPrimary,
+    ),
+    titleSmall: TextStyle(
+      fontFamily: parisineFont,
+      fontSize: 18,
+      color: Colors.black,
+    ),
+
+    ///
+    /// Subtitles
+    ///
+    subtitle1: null,
+    subtitle2: null,
+
+    ///
+    /// Body
+    ///
+    bodyLarge: null,
+    bodyMedium: null,
+    bodySmall: null,
+    bodyText1: null,
+    bodyText2: null,
+
+    ///
+    /// Headlines
+    ///
+    headline1: null,
+    headline2: null,
+    headline3: null,
+    headline4: null,
+    headline5: null,
+    headline6: null,
+    headlineLarge: null,
+    headlineMedium: null,
+    headlineSmall: null,
+
+    ///
+    /// Label
+    ///
+    labelLarge: TextStyle(
+      fontFamily: parisineFont,
+      fontSize: 28,
+      color: Colors.white,
+    ),
+    labelMedium: TextStyle(
+      fontFamily: parisineFont,
+      fontSize: 18,
+      color: Color.fromRGBO(0, 0, 84, 1),
+    ),
+    labelSmall: TextStyle(
+      fontFamily: parisineFont,
+      fontSize: 14,
+      color: Color.fromRGBO(0, 0, 84, 1),
+    ),
+
+    ///
+    /// Display
+    ///
+    displayLarge: null,
+    displayMedium: null,
+    displaySmall: null,
   ),
 );
 
